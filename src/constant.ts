548b1d012cb5b64af2354bd92b103f2321e80496
Shinara Eng <rishijash@hotmail.com>
2025-01-20 22:14:31 -0800

    prompt update

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,7 @@
 export const JSON_REQUEST_SYSTEM_PROMPT = `
     You are a helpful assistant that converts code models into JSON with dummy data. 
+    User gives you a code snippet that is basically request payload in coding language.
+    You convert it to JSON format so they can use it to test their APIs.
     Be creative with dummy data.
     Return only valid JSON.
 `;
